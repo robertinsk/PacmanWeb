60bee4e11ac6ec8f352a8a64569cf5718d7013e6
Nacho <nacho.rosales04@gmail.com>
2025-06-02 16:47:59 -0300

    GIROSCOPIO 1.8.0

--- pacman.js
+++ pacman.js
@@ -1412,7 +1412,7 @@ var PACMAN = (function () {
       window.addEventListener('deviceorientation', function(event) {
         console.log('Alpha:', event.alpha);
         if (event.alpha > 100){
-            user.keyDown({ keyCode: KEY.ARROW_RIGHT, preventDefault: () => {}, stopPropagation: () => {} });
+            PACMAN.user.keyDown({ keyCode: KEY.ARROW_RIGHT, preventDefault: () => {}, stopPropagation: () => {} });
         }
         console.log('Beta:', event.beta);
         console.log('Gamma:', event.gamma);
