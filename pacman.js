fa5d0ad4b4a6cd23fcd9ebcb093591b7909afaaa
Nacho <nacho.rosales04@gmail.com>
2025-06-02 14:50:15 -0300

    AGREGADO GIROSCOPIO 1.0.0

--- pacman.js
+++ pacman.js
@@ -1398,6 +1398,59 @@ var PACMAN = (function () {
     
 }());
 
+//.........................................ACA INICIA EL GIROSCOPIO...................................................................................
+
+if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
+    // iOS 13+ requiere permiso
+    DeviceOrientationEvent.requestPermission()
+        .then(permissionState => {
+            if (permissionState === 'granted') {
+                window.addEventListener('deviceorientation', handleOrientation, true);
+            }
+        })
+        .catch(console.error);
+} else {
+    // Android y otros navegadores
+    window.addEventListener('deviceorientation', handleOrientation, true);
+}
+
+function handleOrientation(event) {
+    const gamma = event.gamma; // izquierda (-90) a derecha (+90)
+    const beta = event.beta;   // adelante (0) a atrás (180)
+
+    // Por ejemplo:
+    if (gamma > 15) {
+        // Mover a la derecha
+    } else if (gamma < -15) {
+        // Mover a la izquierda
+    }
+
+    if (beta > 45) {
+        // Mover hacia abajo
+    } else if (beta < 15) {
+        // Mover hacia arriba
+    }
+}
+
+function handleOrientation(event) {
+    const gamma = event.gamma;
+    const beta = event.beta;
+
+    if (Math.abs(gamma) > Math.abs(beta)) {
+        if (gamma > 15) {
+            user.keyDown({ keyCode: KEY.ARROW_RIGHT, preventDefault: () => {}, stopPropagation: () => {} });
+        } else if (gamma < -15) {
+            user.keyDown({ keyCode: KEY.ARROW_LEFT, preventDefault: () => {}, stopPropagation: () => {} });
+        }
+    } else {
+        if (beta > 45) {
+            user.keyDown({ keyCode: KEY.ARROW_DOWN, preventDefault: () => {}, stopPropagation: () => {} });
+        } else if (beta < 15) {
+            user.keyDown({ keyCode: KEY.ARROW_UP, preventDefault: () => {}, stopPropagation: () => {} });
+        }
+    }
+}
+
 /* Human readable keyCode index */
 var KEY = {'BACKSPACE': 8, 'TAB': 9, 'NUM_PAD_CLEAR': 12, 'ENTER': 13, 'SHIFT': 16, 'CTRL': 17, 'ALT': 18, 'PAUSE': 19, 'CAPS_LOCK': 20, 'ESCAPE': 27, 'SPACEBAR': 32, 'PAGE_UP': 33, 'PAGE_DOWN': 34, 'END': 35, 'HOME': 36, 'ARROW_LEFT': 37, 'ARROW_UP': 38, 'ARROW_RIGHT': 39, 'ARROW_DOWN': 40, 'PRINT_SCREEN': 44, 'INSERT': 45, 'DELETE': 46, 'SEMICOLON_REPRECATED': 59, 'WINDOWS_LEFT': 91, 'WINDOWS_RIGHT': 92, 'SELECT': 93, 'NUM_PAD_ASTERISK': 106, 'NUM_PAD_PLUS_SIGN': 107, 'NUM_PAD_HYPHEN-MINUS': 109, 'NUM_PAD_FULL_STOP': 110, 'NUM_PAD_SOLIDUS': 111, 'NUM_LOCK': 144, 'SCROLL_LOCK': 145, 'SEMICOLON': 186, 'EQUALS_SIGN': 187, 'COMMA': 188, 'HYPHEN-MINUS': 189, 'FULL_STOP': 190, 'SOLIDUS': 191, 'GRAVE_ACCENT': 192, 'LEFT_SQUARE_BRACKET': 219, 'REVERSE_SOLIDUS': 220, 'RIGHT_SQUARE_BRACKET': 221, 'APOSTROPHE': 222};
 
